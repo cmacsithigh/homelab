88a9ce136af496a974726f2877234d61e1a50b79
Cian Sheehy <csheehy@pm.me>
2026-08-08 19:54:08 +0100

    feat: simplify code

--- images/mympd/scripts/cmd.sh
+++ images/mympd/scripts/cmd.sh
@@ -6,10 +6,16 @@ chmod -R 777 /var/lib/mympd
 chown -R 1000:1000 /var/cache/mympd
 chmod -R 777 /var/cache/mympd
 
+mkdir -p /var/lib/mpd/playlists
+mkdir -p /var/lib/mympd/pics/playlists
+
+cp -a /playlists/. /var/lib/mpd/playlists/ 2>/dev/null || true
+
 chown -R 1000:1000 /var/lib/mympd/pics/thumbs
 chmod -R 777 /var/lib/mympd/pics/thumbs
 
 chown -R 1000:1000 /var/lib/mympd/scripts
 chmod -R 777 /var/lib/mympd/scripts
+cp -a /lua_scripts/. /var/lib/mympd/scripts/ 2>/dev/null || true
 
-mv /var/lib/mpd/playlists/*.{jpg,png} /var/lib/mympd/pics/playlists/
+mv /var/lib/mpd/pictures/*.{jpg,png} /var/lib/mympd/pics/playlists/ 2>/dev/null || true
