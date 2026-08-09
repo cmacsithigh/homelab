e89732d22d5175a3ce6cab40d96edd2f30c41269
Cian Sheehy <csheehy@pm.me>
2026-08-09 15:48:11 +0100

    fix: update host

--- images/mympd/scripts/cmd.sh
+++ images/mympd/scripts/cmd.sh
@@ -9,14 +9,10 @@ chmod -R 777 /var/cache/mympd
 mkdir -p /var/lib/mpd/playlists
 mkdir -p /var/lib/mympd/pics/playlists
 
-cp -a /playlists/. /var/lib/mpd/playlists/ 2>/dev/null || true
-
 chown -R 1000:1000 /var/lib/mympd/pics/thumbs
 chmod -R 777 /var/lib/mympd/pics/thumbs
 
-mkdir -p /var/lib/mympd/scripts
 chown -R 1000:1000 /var/lib/mympd/scripts
 chmod -R 777 /var/lib/mympd/scripts
-cp -a /lua_scripts/. /var/lib/mympd/scripts/ 2>/dev/null || true
 
 mv /var/lib/mpd/pictures/*.{jpg,png} /var/lib/mympd/pics/playlists/ 2>/dev/null || true
---
+++ images/mympd/scripts/copy-files.sh
@@ -0,0 +1,2 @@
+cp -a /playlists/. /var/lib/mpd/playlists/ 2>/dev/null || true
+cp -a /lua_scripts/. /var/lib/mympd/scripts/ 2>/dev/null || true
