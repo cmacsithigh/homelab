dcbf6d47cef7fb959a90540b27f7a8f8c0514a64
Cian Sheehy <csheehy@pm.me>
2026-08-09 19:38:28 +0100

    fix: ensure pictures are added

--- images/mympd/scripts/cmd.sh
+++ images/mympd/scripts/cmd.sh
@@ -6,12 +6,8 @@ chmod -R 777 /var/lib/mympd
 chown -R 1000:1000 /var/cache/mympd
 chmod -R 777 /var/cache/mympd
 
-mkdir -p /var/lib/mympd/pics/playlists
-
 chown -R 1000:1000 /var/lib/mympd/pics/thumbs
 chmod -R 777 /var/lib/mympd/pics/thumbs
 
 chown -R 1000:1000 /var/lib/mympd/scripts
 chmod -R 777 /var/lib/mympd/scripts
-
-mv /var/lib/mpd/pictures/*.{jpg,png} /var/lib/mympd/pics/playlists/ 2>/dev/null || true
--- images/mympd/scripts/copy-files.sh
+++ images/mympd/scripts/copy-files.sh
@@ -1,2 +1,6 @@
 cp -a /playlists/. /var/lib/mpd/playlists/ 2>/dev/null || true
 cp -a /lua_scripts/. /var/lib/mympd/scripts/ 2>/dev/null || true
+
+
+mkdir -p /var/lib/mympd/pics/playlists
+cp -a /pictures/. /var/lib/mympd/pics/playlists/ 2>/dev/null || true
