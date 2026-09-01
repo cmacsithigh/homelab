cc553fbbc132d61799adecfe5305341405103360
Cian Sheehy <csheehy@pm.me>
2026-09-01 20:40:36 +0100

    fix: ensure mympd works

--- images/mympd/scripts/copy-files.sh
+++ images/mympd/scripts/copy-files.sh
@@ -6,3 +6,6 @@ mkdir -p /var/lib/mympd/pics/playlists
 cp -a /pictures/. /var/lib/mympd/pics/playlists/ 2>/dev/null || true
 
 cp -a /state/. /var/lib/mympd/state/ 2>/dev/null || true
+
+chown -R 1000:1000 /var/lib/mympd /var/cache/mympd /var/lib/mympd/pics/thumbs /var/lib/mympd/scripts
+chmod -R 777 /var/lib/mympd /var/cache/mympd /var/lib/mympd/pics/thumbs /var/lib/mympd/scripts
